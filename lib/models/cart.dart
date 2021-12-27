4ccef6d4b3c325c586fbea2977e1525cdcec1180
Zack Cauchon <zcauchon@gmail.com>
2021-12-26 22:21:52 -0500

    updating cart quantity

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -4,16 +4,30 @@ import 'package:flutter/cupertino.dart';
 import 'package:ono_frontend/models/product.dart';
 
 class CartModel extends ChangeNotifier {
-  final List<Product> _items = [];
+  late Product product;
+  late num quantity = 0;
 
-  UnmodifiableListView<Product> get items => UnmodifiableListView(_items);
+  CartModel();
+  CartModel.newProduct(this.product, this.quantity);
+
+  final List<CartModel> _items = [];
+
+  UnmodifiableListView<CartModel> get items => UnmodifiableListView(_items);
 
   void add(Product item) {
-    _items.add(item);
+    CartModel existinItem = _items.firstWhere(
+        (element) => element.product.title == item.title,
+        orElse: () => CartModel());
+    if (existinItem.quantity == 0) {
+      _items.add(CartModel.newProduct(item, 1));
+    } else {
+      existinItem.quantity++;
+    }
     notifyListeners();
   }
 
-  void delete(Product item) {
+  void delete(CartModel item) {
     _items.remove(item);
+    notifyListeners();
   }
 }
--- lib/views/cartpage.dart
+++ lib/views/cartpage.dart
@@ -3,16 +3,11 @@ import 'package:ono_frontend/models/cart.dart';
 import 'package:ono_frontend/views/ono_app_bar.dart';
 import 'package:provider/provider.dart';
 
-class CartPage extends StatefulWidget {
+class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
 
   static const String cartPageRoute = '/cart';
 
-  @override
-  State<CartPage> createState() => _CartPageState();
-}
-
-class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
     var cart = context.watch<CartModel>();
@@ -20,20 +15,28 @@ class _CartPageState extends State<CartPage> {
     return Scaffold(
       appBar: const OnoAppBar(title: 'Overnight Office'),
       body: cart.items.isNotEmpty
-          ? ListView.builder(
-              itemCount: cart.items.length,
-              itemBuilder: (context, index) => Center(
-                child: ListTile(
-                  leading: Image.network(cart.items[index].image),
-                  title: Text(cart.items[index].title),
-                  subtitle: Text(cart.items[index].price),
-                  trailing: TextButton(
-                      onPressed: () {
-                        setState(() {
+          ? Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(10),
+              child: ListView.builder(
+                itemCount: cart.items.length,
+                itemBuilder: (context, index) => Center(
+                  child: ListTile(
+                    leading: Image.network(cart.items[index].product.image),
+                    title: Text('\$${cart.items[index].product.title}'),
+                    subtitle: Row(
+                      children: [
+                        Text(cart.items[index].quantity.toString()),
+                        const Text(' @ \$'),
+                        Text(cart.items[index].product.price),
+                      ],
+                    ),
+                    trailing: TextButton(
+                        onPressed: () {
                           cart.delete(cart.items[index]);
-                        });
-                      },
-                      child: const Text('Remove')),
+                        },
+                        child: const Text('Remove')),
+                  ),
                 ),
               ),
             )
